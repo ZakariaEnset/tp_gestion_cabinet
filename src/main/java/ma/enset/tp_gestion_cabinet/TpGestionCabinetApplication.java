f2957633d12a943409ac007df9cce966b780a8d2
ZakariaBouskif <z.bouskif@gmail.com>
2026-03-03 22:06:25 +0000

    initial project setup

---
+++ src/main/java/ma/enset/tp_gestion_cabinet/TpGestionCabinetApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.tp_gestion_cabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGestionCabinetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpGestionCabinetApplication.class, args);
+    }
+
+}
